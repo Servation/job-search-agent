bcbd5ed4772ef81c861f8acec6a49d3a349700fc
Jeffrey Saelee <gwfds123@gmail.com>
2026-05-27 16:03:03 -0700

    Include scannedJobs in backend sourcing blacklist payload to yield 100% fresh sourcing results

--- src/components/JobScanner.tsx
+++ src/components/JobScanner.tsx
@@ -324,6 +324,7 @@ export default function JobScanner({
           savedJobs: [
             ...savedJobs,
             ...watchlist,
+            ...scannedJobs,
             ...mappedDismissed
           ],
         }),
